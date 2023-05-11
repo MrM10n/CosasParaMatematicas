717b8d6416930af3092a1061aa242b2264cf0e5c
Mr.Man <128426838+MrM10n@users.noreply.github.com>
2023-05-11 07:12:38 -0600

    CALCULADORA
    Sirve  para darle dos números enteros y que te los sume divida multiplique y reste.

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+numero1 = int(input("Dame un numero entero: "))
+numero2 = int(input("Dame otro: "))
+
+print("------------------------------------------------------")
+print (f"La suma de estos numeros es {numero1 + numero2}")
+print("------------------------------------------------------")
+print (f"Las restas serían {numero1 - numero2} o al revés {numero2 - numero1}")
+print("------------------------------------------------------")
+print (f"Divididos serían {numero1 / numero2} y al revés {numero2 / numero1}")
+print("------------------------------------------------------")
+print (f"La multiplicacion sería {numero1 * numero2}")
+print("------------------------------------------------------")
